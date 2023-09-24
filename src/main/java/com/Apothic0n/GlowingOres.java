401bdb6197561cbf686a53401c66b9978a3a3ed2
Apothicon02 <79332745+Apothicon02@users.noreply.github.com>
2023-09-24 14:08:57 -0400

    Initial commit

---
+++ src/main/java/com/Apothic0n/GlowingOres.java
@@ -0,0 +1,12 @@
+package com.Apothic0n;
+
+
+import net.fabricmc.api.ModInitializer;
+
+public class GlowingOres implements ModInitializer {
+    public static final String MODID = "glore";
+    @Override
+    public void onInitialize() {
+
+    }
+}
