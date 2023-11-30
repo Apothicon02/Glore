8428727e9bf641307c85e06dfd4761d701f1c340
Apothicon02 <79332745+Apothicon02@users.noreply.github.com>
2023-11-30 14:20:10 -0500

    - Updated to 1.0.1. - Fixed not working on Forge. - Mod rewritten for better compatibility.

--- src/main/java/com/Apothic0n/GlowingOres/GlowingOres.java
+++ src/main/java/com/Apothic0n/GlowingOres/GlowingOres.java
@@ -1,8 +1,12 @@
 package com.Apothic0n.GlowingOres;
 
 
+import com.google.common.collect.ImmutableList;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(GlowingOres.MODID)
@@ -11,5 +15,29 @@ public class GlowingOres {
 
     public GlowingOres() {
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::commonSetup);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        event.enqueueWork(() -> {
+            addLight(Blocks.DIAMOND_ORE.getStateDefinition().getPossibleStates(), 15);
+            addLight(Blocks.DEEPSLATE_DIAMOND_ORE.getStateDefinition().getPossibleStates(), 15);
+            addLight(Blocks.EMERALD_ORE.getStateDefinition().getPossibleStates(), 12);
+            addLight(Blocks.DEEPSLATE_EMERALD_ORE.getStateDefinition().getPossibleStates(), 12);
+            addLight(Blocks.GOLD_ORE.getStateDefinition().getPossibleStates(), 10);
+            addLight(Blocks.DEEPSLATE_GOLD_ORE.getStateDefinition().getPossibleStates(), 10);
+            addLight(Blocks.LAPIS_ORE.getStateDefinition().getPossibleStates(), 7);
+            addLight(Blocks.DEEPSLATE_LAPIS_ORE.getStateDefinition().getPossibleStates(), 7);
+            addLight(Blocks.IRON_ORE.getStateDefinition().getPossibleStates(), 4);
+            addLight(Blocks.IRON_ORE.getStateDefinition().getPossibleStates(), 4);
+            addLight(Blocks.COAL_ORE.getStateDefinition().getPossibleStates(), 4);
+            addLight(Blocks.COAL_ORE.getStateDefinition().getPossibleStates(), 4);
+        });
+    }
+
+    private void addLight(ImmutableList<BlockState> blockStates, int light) {
+        for (int i = 0; i < blockStates.size(); i++) {
+            blockStates.get(i).lightEmission = light;
+        }
     }
 }
