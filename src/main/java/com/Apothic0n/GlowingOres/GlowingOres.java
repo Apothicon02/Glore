ce03aa2e084927c0fe6f8e88355afdf2de4e3a60
Apothicon02 <79332745+Apothicon02@users.noreply.github.com>
2024-02-29 09:54:08 -0500

    - Updated to 1.2.0. - Added a config to change what blocks glow. - Added a config to change what brightness each block should glow at.

--- src/main/java/com/Apothic0n/GlowingOres/GlowingOres.java
+++ src/main/java/com/Apothic0n/GlowingOres/GlowingOres.java
@@ -2,38 +2,33 @@ package com.Apothic0n.GlowingOres;
 
 
 import com.google.common.collect.ImmutableList;
-import net.minecraft.world.level.block.Blocks;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
+import static com.Apothic0n.GlowingOres.GloreJsonReader.customBlocks;
+import static com.Apothic0n.GlowingOres.GloreJsonReader.customBrightness;
+
 @Mod(GlowingOres.MODID)
 public class GlowingOres {
     public static final String MODID = "glore";
 
-    public GlowingOres() {
+    public GlowingOres() throws Exception {
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::commonSetup);
+
+        GloreJsonReader.main();
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
         event.enqueueWork(() -> {
-            addLight(Blocks.DIAMOND_ORE.getStateDefinition().getPossibleStates(), 15);
-            addLight(Blocks.DEEPSLATE_DIAMOND_ORE.getStateDefinition().getPossibleStates(), 15);
-            addLight(Blocks.EMERALD_ORE.getStateDefinition().getPossibleStates(), 12);
-            addLight(Blocks.DEEPSLATE_EMERALD_ORE.getStateDefinition().getPossibleStates(), 12);
-            addLight(Blocks.GOLD_ORE.getStateDefinition().getPossibleStates(), 10);
-            addLight(Blocks.DEEPSLATE_GOLD_ORE.getStateDefinition().getPossibleStates(), 10);
-            addLight(Blocks.LAPIS_ORE.getStateDefinition().getPossibleStates(), 7);
-            addLight(Blocks.DEEPSLATE_LAPIS_ORE.getStateDefinition().getPossibleStates(), 7);
-            addLight(Blocks.COPPER_ORE.getStateDefinition().getPossibleStates(), 7);
-            addLight(Blocks.DEEPSLATE_COPPER_ORE.getStateDefinition().getPossibleStates(), 7);
-            addLight(Blocks.IRON_ORE.getStateDefinition().getPossibleStates(), 4);
-            addLight(Blocks.IRON_ORE.getStateDefinition().getPossibleStates(), 4);
-            addLight(Blocks.COAL_ORE.getStateDefinition().getPossibleStates(), 4);
-            addLight(Blocks.COAL_ORE.getStateDefinition().getPossibleStates(), 4);
+            for (int i = 0; i < customBlocks.size(); i++) {
+                addLight(BuiltInRegistries.BLOCK.get(new ResourceLocation(customBlocks.get(i))).getStateDefinition().getPossibleStates(), customBrightness.get(i));
+            }
         });
     }
 
