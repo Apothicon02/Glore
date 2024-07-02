152ae49810b93e5ba62167ed78d9d431b37aed40
Apothicon02 <79332745+Apothicon02@users.noreply.github.com>
2024-07-02 13:00:19 -0400

    - Updated to 1.3.0. - Removed the old config. - Added a new and improved config in the configs folder. - Removed fabric api dependency.

--- src/main/java/com/Apothic0n/GlowingOres.java
+++ src/main/java/com/Apothic0n/GlowingOres.java
@@ -1,13 +1,10 @@
 package com.Apothic0n;
 
-import com.google.common.collect.ImmutableList;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.block.state.BlockState;
 
 import static com.Apothic0n.GloreJsonReader.customBlocks;
-import static com.Apothic0n.GloreJsonReader.customBrightness;
 
 public class GlowingOres implements ModInitializer {
     public static final String MODID = "glore";
@@ -18,15 +15,6 @@ public class GlowingOres implements ModInitializer {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        for (int i = 0; i < customBlocks.size(); i++) {
-            addLight(BuiltInRegistries.BLOCK.get(new ResourceLocation(customBlocks.get(i))).getStateDefinition().getPossibleStates(), customBrightness.get(i));
-        }
-    }
-
-
-    private void addLight(ImmutableList<BlockState> blockStates, int light) {
-        for (int i = 0; i < blockStates.size(); i++) {
-            blockStates.get(i).lightEmission = light;
-        }
+        customBlocks.forEach((block, brightness) -> BuiltInRegistries.BLOCK.get(ResourceLocation.parse(block)).getStateDefinition().getPossibleStates().forEach((state -> state.lightEmission = brightness)));
     }
 }
