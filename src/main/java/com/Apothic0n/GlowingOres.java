7d3089390df5700f2cac31377486d27ff1164a80
Apothicon02 <79332745+Apothicon02@users.noreply.github.com>
2024-09-21 10:47:43 -0400

    - Updated to 1.4.1. - Added an exception for when users add blocks that do not exist or are incompatible instead of having a mysterious crash.

--- src/main/java/com/Apothic0n/GlowingOres.java
+++ src/main/java/com/Apothic0n/GlowingOres.java
@@ -3,6 +3,8 @@ package com.Apothic0n;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 
 import static com.Apothic0n.GloreJsonReader.customBlocks;
 
@@ -15,6 +17,14 @@ public class GlowingOres implements ModInitializer {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        customBlocks.forEach((block, brightness) -> BuiltInRegistries.BLOCK.get(ResourceLocation.parse(block)).getStateDefinition().getPossibleStates().forEach((state -> state.lightEmission = brightness)));
+        //customBlocks.forEach((block, brightness) -> BuiltInRegistries.BLOCK.get(ResourceLocation.parse(block)).getStateDefinition().getPossibleStates().forEach((state -> state.lightEmission = brightness)));
+        customBlocks.forEach((block, brightness) -> {
+            Block registeredBlock = BuiltInRegistries.BLOCK.get(ResourceLocation.parse(block));
+            if (registeredBlock == Blocks.AIR) {
+                throw new RuntimeException("Could not locate "+block+", this could be due to a typo in the block name or the mod the block is added by being incompatible with Glore's method of making blocks emit light.");
+            } else {
+                registeredBlock.getStateDefinition().getPossibleStates().forEach((state -> state.lightEmission = brightness));
+            }
+        });
     }
 }
