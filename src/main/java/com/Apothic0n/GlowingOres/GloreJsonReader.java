8cf4813f131ac94a29bc311738c969700b877c9f
Apothicon02 <79332745+Apothicon02@users.noreply.github.com>
2024-09-21 11:52:33 -0400

    - Updated to 1.4.1. - Added an exception for when users add blocks that do not exist or are incompatible instead of having a mysterious crash. The following changes were already applied to the Fabric version and are only new to the Forge port: - Ported to 1.21. - Removed the old config. - Added a new and improved config in the configs folder. - Added an iris-exclusive feature where armor trim materials can be set to emit light via a new config file. - Redstone armor trims now emit light when using iris shaders. - Amethyst armor trims now emit light when using iris shaders. - Fixed mod icon not being used.

--- src/main/java/com/Apothic0n/GlowingOres/GloreJsonReader.java
+++ src/main/java/com/Apothic0n/GlowingOres/GloreJsonReader.java
@@ -1,61 +1,66 @@
 package com.Apothic0n.GlowingOres;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
+import com.google.gson.*;
 import com.google.gson.stream.JsonReader;
-import com.google.gson.stream.JsonWriter;
 import net.minecraftforge.fml.loading.FMLPaths;
 
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
 
 public class GloreJsonReader {
-    public static List<String> customBlocks;
-    public static List<Integer> customBrightness;
+    public static Map<String, Integer> customBlocks;
+    public static Map<String, Integer> customTrims;
     public static void main() throws Exception {
-        makeCustomBlocks(Path.of(FMLPaths.MODSDIR.get().toString() + "/glore_blocks.json"));
-        makeCustomBrightness(Path.of(FMLPaths.MODSDIR.get().toString() + "/glore_brightness.json"));
+        makeConfig(Path.of(FMLPaths.CONFIGDIR.get() + "/glore.json"));
+        makeTrimsConfig(Path.of(FMLPaths.CONFIGDIR.get() + "/glore_trims.json"));
     }
 
-    private static void makeCustomBlocks(Path path) throws IOException {
-        Gson gson = new Gson();
+    public static void makeConfig(Path path) throws IOException {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
         if (!Files.exists(path)) {
-            JsonWriter writer = new JsonWriter(new FileWriter(path.toString()));
-            JsonObject defaultData = gson.fromJson("{\"values\":[\"minecraft:coal_ore\",\"minecraft:deepslate_coal_ore\",\"minecraft:iron_ore\",\"minecraft:deepslate_iron_ore\",\"minecraft:copper_ore\",\"minecraft:deepslate_copper_ore\",\"minecraft:lapis_ore\",\"minecraft:deepslate_lapis_ore\",\"minecraft:gold_ore\",\"minecraft:deepslate_gold_ore\",\"minecraft:emerald_ore\",\"minecraft:deepslate_emerald_ore\",\"minecraft:diamond_ore\",\"minecraft:deepslate_diamond_ore\"]}", JsonObject.class);
-            gson.toJson(defaultData, writer);
-            writer.close();
+            Files.write(path, ("{\n" +
+                    "  \"minecraft:coal_ore\":4,\n" +
+                    "  \"minecraft:deepslate_coal_ore\":4,\n" +
+                    "  \"minecraft:iron_ore\":4,\n" +
+                    "  \"minecraft:deepslate_iron_ore\":4,\n" +
+                    "  \"minecraft:copper_ore\":7,\n" +
+                    "  \"minecraft:deepslate_copper_ore\":7,\n" +
+                    "  \"minecraft:lapis_ore\":7,\n" +
+                    "  \"minecraft:deepslate_lapis_ore\":7,\n" +
+                    "  \"minecraft:gold_ore\":10,\n" +
+                    "  \"minecraft:deepslate_gold_ore\":10,\n" +
+                    "  \"minecraft:emerald_ore\":12,\n" +
+                    "  \"minecraft:deepslate_emerald_ore\":12,\n" +
+                    "  \"minecraft:diamond_ore\":15,\n" +
+                    "  \"minecraft:deepslate_diamond_ore\":15\n" +
+                    "}").getBytes());
         }
         JsonReader reader = new JsonReader(new FileReader(path.toString()));
         JsonObject data = gson.fromJson(reader, JsonObject.class);
-        JsonArray customBlockNames = data.get("values").getAsJsonArray();
-        List<String> tempCustomBlocks = new ArrayList<>(List.of());
-        for (int i = 0; i < customBlockNames.size(); i++) {
-            tempCustomBlocks.add(customBlockNames.get(i).getAsString());
-        }
+
+        Map<String, Integer> tempCustomBlocks = new java.util.HashMap<>(Map.of());
+        data.asMap().forEach((block, brightness) -> tempCustomBlocks.put(block, brightness.getAsInt()));
+
         customBlocks = tempCustomBlocks;
     }
 
-    private static void makeCustomBrightness(Path path) throws IOException {
-        Gson gson = new Gson();
+    public static void makeTrimsConfig(Path path) throws IOException {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
         if (!Files.exists(path)) {
-            JsonWriter writer = new JsonWriter(new FileWriter(path.toString()));
-            JsonObject defaultData = gson.fromJson("{\"values\":[4,4,4,4,7,7,7,7,10,10,12,12,15,15]}", JsonObject.class);
-            gson.toJson(defaultData, writer);
-            writer.close();
+            Files.write(path, ("{\n" +
+                    "  \"amethyst\":4,\n" +
+                    "  \"redstone\":2\n" +
+                    "}").getBytes());
         }
         JsonReader reader = new JsonReader(new FileReader(path.toString()));
         JsonObject data = gson.fromJson(reader, JsonObject.class);
-        JsonArray gloreCustomBrightness = data.get("values").getAsJsonArray();
-        List<Integer> tempCustomBrightness = new ArrayList<>(List.of());
-        for (int i = 0; i < gloreCustomBrightness.size(); i++) {
-            tempCustomBrightness.add(gloreCustomBrightness.get(i).getAsInt());
-        }
-        customBrightness = tempCustomBrightness;
+
+        Map<String, Integer> tempCustomTrims = new java.util.HashMap<>(Map.of());
+        data.asMap().forEach((trim, brightness) -> tempCustomTrims.put(trim, brightness.getAsInt()));
+
+        customTrims = tempCustomTrims;
     }
 }
--- src/main/java/com/Apothic0n/GlowingOres/GlowingOres.java
+++ src/main/java/com/Apothic0n/GlowingOres/GlowingOres.java
@@ -1,40 +1,31 @@
 package com.Apothic0n.GlowingOres;
 
-
-import com.google.common.collect.ImmutableList;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 import static com.Apothic0n.GlowingOres.GloreJsonReader.customBlocks;
-import static com.Apothic0n.GlowingOres.GloreJsonReader.customBrightness;
 
 @Mod(GlowingOres.MODID)
 public class GlowingOres {
     public static final String MODID = "glore";
 
     public GlowingOres() throws Exception {
-        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::commonSetup);
+        try {
+            GloreJsonReader.main();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
-        GloreJsonReader.main();
-    }
-
-    private void commonSetup(final FMLCommonSetupEvent event) {
-        event.enqueueWork(() -> {
-            for (int i = 0; i < customBlocks.size(); i++) {
-                addLight(BuiltInRegistries.BLOCK.get(new ResourceLocation(customBlocks.get(i))).getStateDefinition().getPossibleStates(), customBrightness.get(i));
+        customBlocks.forEach((block, brightness) -> {
+            Block registeredBlock = BuiltInRegistries.BLOCK.get(ResourceLocation.parse(block));
+            if (registeredBlock == Blocks.AIR) {
+                throw new RuntimeException("Could not locate "+block+", this could be due to a typo in the block name or the mod the block is added by being incompatible with Glore's method of making blocks emit light.");
+            } else {
+                registeredBlock.getStateDefinition().getPossibleStates().forEach((state -> state.lightEmission = brightness));
             }
         });
-    }
-
-    private void addLight(ImmutableList<BlockState> blockStates, int light) {
-        for (int i = 0; i < blockStates.size(); i++) {
-            blockStates.get(i).lightEmission = light;
-        }
     }
 }
