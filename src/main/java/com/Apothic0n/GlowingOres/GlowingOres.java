d0c5897854cfd4a1b77d3d3429ae74fbf680c459
Apothicon02 <79332745+Apothicon02@users.noreply.github.com>
2024-09-21 14:32:25 -0400

    - Fixed crash.

--- src/main/java/com/Apothic0n/GlowingOres/GlowingOres.java
+++ src/main/java/com/Apothic0n/GlowingOres/GlowingOres.java
@@ -1,10 +1,10 @@
 package com.Apothic0n.GlowingOres;
 
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ForgeRegistries;
 
 import static com.Apothic0n.GlowingOres.GloreJsonReader.customBlocks;
 
@@ -20,8 +20,8 @@ public class GlowingOres {
         }
 
         customBlocks.forEach((block, brightness) -> {
-            Block registeredBlock = BuiltInRegistries.BLOCK.get(ResourceLocation.parse(block));
-            if (registeredBlock == Blocks.AIR) {
+            Block registeredBlock = ForgeRegistries.BLOCKS.getValue(ResourceLocation.parse(block));
+            if (registeredBlock == null || registeredBlock == Blocks.AIR) {
                 throw new RuntimeException("Could not locate "+block+", this could be due to a typo in the block name or the mod the block is added by being incompatible with Glore's method of making blocks emit light.");
             } else {
                 registeredBlock.getStateDefinition().getPossibleStates().forEach((state -> state.lightEmission = brightness));
